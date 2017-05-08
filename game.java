c78009168345aeba2e005e286d6efd3bbc95fc95
Huntforth <cpeterson11@pvlearners.net>
2017-05-08 08:12:15 -0700

    Update game.java

--- game.java
+++ game.java
@@ -1,19 +1,20 @@
 import java.util.Scanner;
 import java.lang.Math;
 
-class main 
+class game 
 {
-  Scanner scanner = new Scanner(System.in);
-  boolean flag = true;
-  
-  System.out.println("Guess 1 or 0");
-  while (flag)
-  {
-    System.out.println("Guess?");
-    int answer = scanner.nextInt();
-    int comp = (Math.random * 2) / 1;
-    System.out.println(comp);
-    if (answer != comp)
-      flag = false;
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    boolean flag = true;
+    
+    System.out.println("Guess 1 or 0");
+    while (flag) {
+      System.out.println("Guess?");
+      int answer = scanner.nextInt();
+      int comp = (int) (Math.random() * 2) / 1;
+      System.out.println(comp);
+      if (answer != comp)
+        flag = false;
+    }
   }
 }
