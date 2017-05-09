7f6c8517437eec59567263f12ef084e2fc61415d
Huntforth <cpeterson11@pvlearners.net>
2017-05-09 08:19:00 -0700

    Update game.java

--- game.java
+++ game.java
@@ -1,32 +1,38 @@
 import java.util.Scanner;
 import java.lang.Math;
+import java.io.*;
 
 class game 
 {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-    boolean flag = true;
-    int c = 0;
-   
-    while (flag) {
-      System.out.println("Guess? (0 or 1)");
+    boolean restart = false;
+    
+    while(!restart)
+    {
+      boolean flag = true;
+      int c = 0;
       
-      int answer = scanner.nextInt();
-      int comp = (int) (Math.random() * 2) / 1;
-     
-      if (answer == 0 || answer == 1)
-      {
-        if (answer == comp)
+      while (flag) {
+        System.out.println("Guess? (0 or 1)");
+        
+        int answer = scanner.nextInt();
+        int comp = (int) (Math.random() * 2) / 1;
+        
+        if (answer == 0 || answer == 1)
         {
-          c++;
-          System.out.println("Points: " + c);
-        }
-        else
-        {
-          flag = false;
+          if (answer == comp)
+          {
+            c++;
+            System.out.println("Points: " + c);
+          }
+          else
+          {
+            flag = false;
+          }
         }
       }
+      System.out.println("\n\n\n\n\n\n\nGAME OVER\nPoints: " + c + "\n\n\n\n\n\n\n");
     }
-    System.out.println("GAME OVER/nPoints: " + c);
   } 
 }
